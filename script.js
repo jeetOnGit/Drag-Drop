b46e2b1b28d7ccaa11a45770a8311f50aa01fbf0
Jeet Das <126141713+jeetOnGit@users.noreply.github.com>
2024-06-25 23:49:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+
+let leftBox = document.getElementById("left")
+let rightBox = document.getElementById("right")
+let lists = document.getElementsByClassName("list")
+
+
+for(list of lists){
+  list.addEventListener("dragstart", function (e) {
+    let selected = e.target;
+    
+    rightBox.addEventListener("dragover", function (e){
+      e.preventDefault();
+    })
+    rightBox.addEventListener("drop", function (e){
+      rightBox.appendChild(selected)
+      selected = null
+    })
+
+    leftBox.addEventListener("dragover", function (e){
+      e.preventDefault();
+    })
+    leftBox.addEventListener("drop", function (e){
+      leftBox.appendChild(selected)
+      selected = null
+    })
+  })
+}
+console.log(rightBox);
